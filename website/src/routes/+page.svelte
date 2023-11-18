8717ad0279dca74c7c45ab38475e4daa616f2820
JackAida <joshuagrant@hotmail.com>
2023-11-19 12:07:14 +1300

    Updating most recent changes: Changes to be committed:     modified:   website/src/routes/+page.svelte

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -10,17 +10,25 @@
         </div>
     </div>
     <div class="content">
-        <div class="border-slate-400 border-b-2 p-1 border-opacity-75 grid grid-cols-4 gap-4 mb-5 pb-3 pl-4 pr-4">
+        <div class="border-slate-400 border-b-2 p-1 grid grid-cols-4 gap-4 mb-5 pb-3 pl-4 pr-4">
             <button class="text-xl bg-orange-700 hover:bg-orange-500 rounded transition-all duration-300 p-1">Plans</button>
             <button class="text-xl bg-orange-700 hover:bg-orange-500 rounded transition-all duration-300">Tools</button>
             <button class="text-xl bg-orange-700 hover:bg-orange-500 rounded transition-all duration-300">Updates</button>
             <button class="text-xl bg-orange-700 hover:bg-orange-500 rounded transition-all duration-300">About</button>
         </div>
         <div class="h-56 grid grid-cols-2 gap-4 content-start">
-            <div class="border-slate-400 border-r-2 border-opacity-75 text-xl">
+            <div class="border-slate-400 border-r-2 text-xl">
                 <p class="text-xl underline underline-offset-4 hover:text-blue-400 duration-150"> <a data-sveltekit-preload-data="tap" href="https://chromewebstore.google.com/detail/mobile-roads-enhanced/epimgdnmjciljnceicmlijgkfbeidfil">Mobile Roads Enhanced</a></p>
             </div>
-            <div class="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at imperdiet turpis, vestibulum aliquam tortor. Integer faucibus neque quam, sit amet euismod nulla egestas sed. Nullam iaculis sapien ut ullamcorper pretium. Vivamus eleifend felis maximus tortor malesuada accumsan. Donec ac posuere felis, eu sodales leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Orci varius.</div>
+            <div class="text-l">Mobile Roads Enhanced is a tool developed to assist Traffic Management Designers/Planners in New Zealand streamline their workflow. I am currently working on getting this to do various other things such as include the NSLR (National Speed Limit Register), TMC (Traffic Management Coordinator) details for each region, including both Local Roads and State Highways which will have to be constantly updated with the help of the Traffic Management Community.
+            </div>
+            <div class="border-slate-400 border-r-2 text-xl">
+                <div class="horizontal-splitter"></div>
+                agdsa
+            </div>
+            <div class="text-l horizontal-splitter">
+                agdsa
+            </div>
         </div>
     </div>
 
@@ -45,5 +53,12 @@
         max-width: 1200px;
         margin-left: auto;
         margin-right: auto;
+    }
+    .horizontal-splitter{
+        width: 95%;
+        margin-left: auto;
+        margin-right: auto;
+        padding-top: auto;
+        border-top: 2px #94a3b8 dashed;
     }
 </style>
