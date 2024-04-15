12d1f1444f24b92330a34cf224d19b6bf0889b4a
JackAida <joshuagrant@hotmail.com>
2024-04-15 12:52:37 +1200

    Added CV route

---
+++ src/routes/cv/+page.svelte
@@ -0,0 +1,12 @@
+<body>
+    <embed src="/joshua_cv.pdf" width="100%" height="100%"/>
+</body>
+
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+        height: 100vh;
+        width: 100vw;
+    }
+</style>
